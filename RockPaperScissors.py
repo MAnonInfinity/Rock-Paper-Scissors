2d20af45d02c877698c5c2ad89be6076654ed244
Manan Varma <mananvarma5401@gmail.com>
2021-01-09 16:07:28 +0530

    Updated
    Corrected for invalid user input

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -30,26 +30,30 @@ scissors = '''
 choicesASCII=[rock, paper, scissors]
 choices=["Rock", "Paper", "Scissors"]
 
-userChoice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+userChoice=input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
 compChoice=random.randint(0, 2)
 
-print(f"You chose : {choices[userChoice]}\n{choicesASCII[userChoice]}\n Computer Chose : {choices[compChoice]}\n{choicesASCII[compChoice]}")
+if userChoice.isdigit()!=True or int(userChoice)>2 or int(userChoice)<0:
+    print("Invalid Choice! You Lose!")
+else:
+    userChoice=int(userChoice)
+    print(f"You chose : {choices[userChoice]}\n{choicesASCII[userChoice]}\n Computer Chose : {choices[compChoice]}\n{choicesASCII[compChoice]}")
 
-if userChoice==compChoice:
-    print("Game Draw!")
-elif userChoice==0:
-    if compChoice==2:
-        print("You Win!")
-    else:
-        print("You Lost!")
-elif userChoice==1:
-    if compChoice==2:
-        print("You Lost!")
-    else:
-        print("You Win!")
-elif userChoice==2:
-    if compChoice==1:
-        print("You Win!")
-    else:
-        print("You Lose!")
+    if userChoice==compChoice:
+        print("Game Draw!")
+    elif userChoice==0:
+        if compChoice==2:
+            print("You Win!")
+        else:
+            print("You Lost!")
+    elif userChoice==1:
+        if compChoice==2:
+            print("You Lost!")
+        else:
+            print("You Win!")
+    elif userChoice==2:
+        if compChoice==1:
+            print("You Win!")
+        else:
+            print("You Lose!")
